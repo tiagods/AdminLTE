484e282b7f545ec9903e594372409112ea1970d5
Tiago Souza <tiagoice@hotmail.com>
2017-11-24 16:59:33 -0200

    First Commit

---
+++ src/main/java/com/plkrhone/sisrh/SisrhApplication.java
@@ -0,0 +1,12 @@
+package com.plkrhone.sisrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisrhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisrhApplication.class, args);
+	}
+}
